a366d8cbe5e9bb1e42379afbb74b978e592fd8ef
John McKie <therealjmac2097@gmail.com>
2017-05-18 16:49:59 +0100

    Mustache added and gallery displaying

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,3 +3,12 @@ bootstrap = require('bootstrap');
 mustache = require('mustache');
 
 
+jQuery(document).ready(function($) {
+    var jqxhr = $.getJSON('data.json', function() {
+
+    }).done(function(data) {
+        var template = $('#template').html();
+        var showTemplate = mustache.render(template, data);
+        $('#gallery').html(showTemplate);
+    });
+});
