9e6c1c2bd9edc0b72c410154009fa07bcb842ba8
John McKie <therealjmac2097@gmail.com>
2017-06-14 15:57:43 +0100

    changes made for production

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,14 +4,6 @@ mustache = require('mustache');
 
 
 jQuery(document).ready(function($) {
-    var jqxhr = $.getJSON('data.json', function() {
-
-    }).done(function(data) {
-        var template = $('#template').html();
-        var showTemplate = mustache.render(template, data);
-        $('#gallery').html(showTemplate);
-    });
-
 
     $('.nav-button').click(function(){
         var txt = $(this).text();
